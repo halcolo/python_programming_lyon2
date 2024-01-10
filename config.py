7e775f46720439a7165634d0ff96f860d1c74030
jdalfons <juandiego.alfonsoocampo@gmail.com>
2024-01-10 18:44:00 +0100

    enhacement TD3-TD10: Changes in cofig img and jupyter deleted :rocket:

--- config.py
+++ config.py
@@ -1,6 +1,11 @@
 import os
 from utils.tools import set_vars
 
+img = {
+    "reddit_logo": "https://github.com/jdalfons/search_engine/assets/25759070/0419f2e0-e103-4a57-9968-b4dcd56226b4",
+    "arxiv_logo": "https://github.com/jdalfons/search_engine/assets/25759070/ae7e2ecf-1d4a-4779-a825-673bb2a83d6d",
+}
+
 CLIENT_SECRET = None
 USER_AGENT = None
 CLIENT_ID = None
@@ -9,6 +14,6 @@ if CLIENT_SECRET is not None and USER_AGENT is not None and CLIENT_ID is not Non
     os.environ["CLIENT_SECRET"] = CLIENT_SECRET 
     os.environ["USER_AGENT"] = USER_AGENT
     os.environ["CLIENT_ID"] =  CLIENT_ID
-    
-# Set environment variables for Reddit authentication.
+
 reddit_auth = set_vars()
+
